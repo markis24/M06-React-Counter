1f432b2b6d8ae65454a2be8a9dc3b17571d7da4f
Marc <2023_marc.cuzcano@iticbcn.cat>
2024-04-08 18:14:37 +0200

    First Commit

---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Boton from "./Boton.jsx";
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Boton/>
+  </React.StrictMode>,
+)
